36565c81a998857b63dd6fd87430ad59be0e701e
Kalpesh <kalpeshsantoki07@gmail.com>
2018-02-15 00:24:57 +0530

    Updated Launcher

--- src/main/java/com/kal/hellowgit2/Launcher.java
+++ src/main/java/com/kal/hellowgit2/Launcher.java
@@ -3,6 +3,6 @@ package com.kal.hellowgit2;
 public class Launcher
 {
     public static void main(String[] args){
-        System.out.println("Hellow");
+        System.out.println("Hellow for module1");
     }
 }
