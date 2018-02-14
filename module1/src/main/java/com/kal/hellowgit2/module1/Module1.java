881161ba9237d164c938d00290f97999c05ae035
Kalpesh <kalpeshsantoki07@gmail.com>
2018-02-15 00:29:46 +0530

    Added Module1

---
+++ module1/src/main/java/com/kal/hellowgit2/module1/Module1.java
@@ -0,0 +1,7 @@
+package com.kal.hellowgit2.module1;
+
+public class Module1 {
+    public void print(){
+        System.out.println("Hellow from Module1");
+    }
+}
--- src/main/java/com/kal/hellowgit2/Launcher.java
+++ src/main/java/com/kal/hellowgit2/Launcher.java
@@ -1,8 +1,12 @@
 package com.kal.hellowgit2;
 
+import com.kal.hellowgit2.module1.Module1;
+
 public class Launcher
 {
     public static void main(String[] args){
         System.out.println("Hellow for module1");
+
+        new Module1().print();
     }
 }
